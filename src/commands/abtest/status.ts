75cb2b718258c0aaf51dfe5d8099fec6e3089108
Thiago Veras <thiago.veras.machado@gmail.com>
2020-07-17 16:47:19 -0300

    remove status command example

--- src/commands/abtest/status.ts
+++ src/commands/abtest/status.ts
@@ -4,8 +4,6 @@ import { CustomCommand } from 'vtex'
 export default class ABTestStatus extends CustomCommand {
   static description = 'Display currently running AB tests results'
 
-  static examples = ['vtex workspace abtest status']
-
   static flags = {
     ...CustomCommand.globalFlags,
   }
