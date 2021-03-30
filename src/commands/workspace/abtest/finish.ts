df07731c15b7ee978b1fe3cd9869f3a079a1ffcc
João Marcos Gris <joao.gris@vtex.com.br>
2021-03-29 22:53:13 -0300

    Update

--- src/commands/workspace/abtest/finish.ts
+++ src/commands/workspace/abtest/finish.ts
@@ -1,13 +1,9 @@
 import testAbFinish from '../../../modules/abtest/finish'
-import { CustomCommand } from 'vtex'
+import CustomCommand from '@oclif/command'
 import { ColorifyConstants } from '../../../utils'
 
 export default class ABTestFinish extends CustomCommand {
   static description = `Stops all A/B tests from running on the current ${ColorifyConstants.ID('account')}.`
-
-  static flags = {
-    ...CustomCommand.globalFlags,
-  }
 
   async run() {
     this.parse(ABTestFinish)
